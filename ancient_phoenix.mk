b5269010326501b0ba0b41e7370332b6081a826f
Bibindasb <75476488+bibindasb@users.noreply.github.com>
2021-03-03 19:12:02 +0530

    Update and rename evolution_phoenix.mk to ancient_phoenix.mk

--- ancient_phoenix.mk
+++ ancient_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common evolution stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_phoenix
+PRODUCT_NAME := ancient_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
