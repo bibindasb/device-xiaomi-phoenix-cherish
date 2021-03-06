6adcfcc9a84c9dd3c32c8ea532d34fdc220ca56d
Bibindasb <75476488+bibindasb@users.noreply.github.com>
2021-03-06 11:04:40 +0530

    Phoenix : Bring Up cherish

--- cherish_phoenix.mk
+++ cherish_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common evolution stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ancient_phoenix
+PRODUCT_NAME := cherish_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
@@ -20,12 +20,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
+CHERISH_WITH_GAPPS := true
 
 # Maintainer Properties
-EVO_BUILD_TYPE := OFFICIAL
-EVO_DONATE_URL := https://www.paypal.me/chiru2000
-EVO_MAINTAINER := CHIRANTH CHIRU
+CHERISH_BUILD_TYPE := OFFICIAL
 
-BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V12.0.2.0.QGHINXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:S/SPP1.210122.022/7158812:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
